a6d8dc244ca5a8c4ed315d798bcec83fb7bb0148
GastonLell <gastonglell@gmail.com>
2021-08-26 21:24:35 -0300

    cart services

--- src/app/components/item-detail/item-detail.component.ts
+++ src/app/components/item-detail/item-detail.component.ts
@@ -3,24 +3,24 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-
+import { IProduct } from 'src/app/models/Product';
 import { ServiceProductsService } from '../../services/products/serviceProducts.service';
-import { CartService } from '../../services/cart/cart.service';
+
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
   styleUrls: ['./item-detail.component.css'],
 })
+
 export class ItemDetailComponent implements OnInit, OnDestroy {
   product: any;
-
   onDestroy$: Subject<boolean> = new Subject();
 
   constructor(
     private route: ActivatedRoute,
     private productService: ServiceProductsService,
-    private CartService: CartService,
+  
   ) {}
 
   getProduct(id: any) {
@@ -28,11 +28,6 @@ export class ItemDetailComponent implements OnInit, OnDestroy {
       .getById(id)
       .pipe(takeUntil(this.onDestroy$))
       .subscribe((res) => (this.product = res));
-  }
-
-  addToCart(producto: any): void{
-    this.CartService.addToCart(producto);
-    window.alert(`Agregaste ${producto.title} al carrito`)
   }
 
   ngOnInit(): void {
--- src/app/services/products/serviceProducts.service.ts
+++ src/app/services/products/serviceProducts.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IProduct } from 'src/app/models/Product';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ServiceProductsService {
   url: string = `https://fakestoreapi.com/products`;
 
   public getAll(){
-    return this.http.get(this.url)
+    return this.http.get<IProduct[]>(this.url)
   }
 
   public getById(id:any){
-    return this.http.get(`${this.url}/${id}`)
+    return this.http.get<IProduct>(`${this.url}/${id}`)
   }
+  
   constructor(
     private http: HttpClient
   ) { }
